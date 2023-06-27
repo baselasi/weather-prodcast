fdee9dc9e79033bfdd1141b7a9e91f94971aa7bd
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-27 23:25:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,34 @@ import SerchBar from './components/serschbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Weather from './components/weatherProdcast';
 import { useState } from 'react';
+import Prodcast from './components/fiveDaysProdcast';
+
 
 function App() {
   const [cordination,setCordination] = useState([])
+  const [prodcast,setProdcast] = useState ([])
   //console.log(cordination)
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'
+        <Route index
             element={<SerchBar
             setCity={setCordination}
             />}
         />
-        <Route path='/weatherProdcast'
+        <Route path='/weatherProdcas'
           element={<Weather
           city={cordination}
+          Prodcast={setProdcast}
           />}
+        >
+        
+        </Route>
+        <Route path='/weatherProdcas/fiveDaysProdcast'
+        element={<Prodcast
+        prodcast={prodcast}
+        city={cordination}
+        />}
         />
       </Routes>
     </BrowserRouter>
